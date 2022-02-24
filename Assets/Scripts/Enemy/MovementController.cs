63ffd3363bcb5a7b9638f290b13e5761b57d534e
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2022-02-24 23:56:13 +0000

    Player Random Movement

--- Assets/Scripts/Enemy/MovementController.cs
+++ Assets/Scripts/Enemy/MovementController.cs
@@ -2,57 +2,49 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Enemy
 {
-    public enum Direction
-    {
-        RIGHT,
-        LEFT,
-        UP,
-        DOWN
-    }
-    
     [RequireComponent(typeof(Rigidbody2D))]
     public class MovementController : MonoBehaviour
     {
         [SerializeField] private float moveDistance;
         [SerializeField] private Direction direction;
+        
         private Rigidbody2D rigidbody2D;
         private SpriteController spriteController;
+        private WallDetector[] wallDetectors = new WallDetector[4];
+            
+        private List<Direction> availableDirections = new List<Direction>();
         private Vector2 velocity;
 
         private void Awake()
         {
             rigidbody2D = GetComponent<Rigidbody2D>();
             spriteController = GetComponent<SpriteController>();
+            for (int i = 0; i < 4; i++)
+            {
+                wallDetectors[i] = transform.GetChild(i + 1).gameObject.GetComponent<WallDetector>();
+            }
         }
 
         public void SetDirection()
         {
             velocity = rigidbody2D.velocity;
-            switch (direction)
-            {
-                case Direction.RIGHT:
-                    velocity.x = 0;
-                    direction = Direction.DOWN;
-                    break;
-                case Direction.LEFT:
-                    velocity.x = 0;
-                    direction = Direction.UP;
-                    break;
-                case Direction.UP:
-                    velocity.y = 0;
-                    direction = Direction.RIGHT;
-                    spriteController.FlipRight();
-                    break;
-                case Direction.DOWN:
-                    velocity.y = 0;
-                    direction = Direction.LEFT;
-                    spriteController.FlipLeft();
-                    break;
-            }
+            velocity.x = 0;
+            velocity.y = 0;
             rigidbody2D.velocity = velocity;
+            availableDirections.Clear();
+            foreach (var wallDetector in wallDetectors)
+            {
+                if (!wallDetector.HasWall())
+                {
+                    availableDirections.Add(wallDetector.GetDirection());   
+                    Debug.Log(wallDetector.GetDirection());
+                }
+            }
+            direction = availableDirections[Random.Range(0, availableDirections.Count)];
         }
 
         private void FixedUpdate()
--- Assets/Scripts/Enemy/WallDetector.cs
+++ Assets/Scripts/Enemy/WallDetector.cs
@@ -6,18 +6,39 @@ using UnityEngine;
 
 public class WallDetector : MonoBehaviour
 {
+    [SerializeField] private Direction direction;
     private MovementController movementController;
+    private bool hasWall;
     
     private void Awake()
     {
         movementController = transform.parent.gameObject.GetComponent<MovementController>();
     }
 
+    public Direction GetDirection()
+    {
+        return direction;
+    }
+
+    public bool HasWall()
+    {
+        return hasWall;
+    }
+
     private void OnTriggerEnter2D(Collider2D collider) 
     {
         if (collider.tag == "Maze")
         {
+            hasWall = true;
             movementController.SetDirection();
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if (collider.tag == "Maze")
+        {
+            hasWall = false;
         }
     }
 }
