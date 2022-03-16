d9af94627b23bf2522b97c04940671c6e414a89e
JordJ123 <71386858+JordJ123@users.noreply.github.com>
2022-03-16 16:25:45 +0000

    Point Achievements

---
+++ Assets/Scripts/Achievement/Point/PointNoKillController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Achievement.Point
+{
+	public class PointNoKillController : AchievementController
+	{
+		[SerializeField] private int pointsValue;
+		
+		private void Awake()
+		{
+			base.Awake();
+			achievement = new Achievement("No Kills Points",
+				string.Format("Collect {0} points without getting a kill in one"
+					+ " game", pointsValue));
+		}
+		
+		private void Start()
+		{
+			if (!achievement.IsUnlocked())
+			{
+				Player.PointController.OnCollect += CheckAchievement;
+				Player.TriggerDetector.OnKill += FailAchievement;
+			}
+		}
+
+		private void CheckAchievement(int pointsValue)
+		{
+			if (pointsValue >= this.pointsValue)
+			{
+				achievementSystem.Unlock(achievement);
+				Player.PointController.OnCollect -= CheckAchievement;
+				Player.TriggerDetector.OnKill -= FailAchievement;
+			}
+		}
+
+		private void FailAchievement()
+		{
+			Player.PointController.OnCollect -= CheckAchievement;
+			Player.TriggerDetector.OnKill -= FailAchievement;
+		}
+	}
+}
